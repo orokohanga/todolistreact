e89338af6b982ce0a4ca5e2e91f8ea8431c656d7
Corto <corto0201@gmail.com>
2024-01-15 13:32:30 +0100

    4

--- src/Task/index.js
+++ src/Task/index.js
@@ -1,17 +1,24 @@
 import {myTasks} from '../Data/data';
 import styles from './App.module.css';
+import { useState } from 'react';
 
 export default function Task() {
+  const [active, setActive] = useState(0)
+
+  function crossTask() {
+    
+  }
 
     return (
       <>
       <h1 className={styles.title}>Mes tâches</h1>
       <div className={styles.task}>
       {myTasks.map(task =>
-        <li 
+        <li
         onDoubleClick={() => console.log(task.name)}
         key={task.id} 
-        className={styles.tasks} 
+        className={styles.tasks}
+        onClick={crossTask}
         id='task'>{task.name}
         </li>
         )}
